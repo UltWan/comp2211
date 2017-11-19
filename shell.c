898e2f20b7136eab33a7cdaaef1c2824fbef8a98
sc16wyrw <sc16wyrw@leeds.ac.uk>
2017-11-19 21:00:34 +0000

    Print working dir

--- shell.c
+++ shell.c
@@ -2,20 +2,28 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <unistd.h>
 
 int main(int argc, char **argv)
 {
-  char command[100];
-  char exit[10] = "exit";
-  char info[10] = "info";
+  char command[50];
+  char exit[5] = "exit";
+  char info[5] = "info";
+  char pwd[4]= "pwd";
+  char cwd[50];
 
   while (strcmp(command, exit) != 0){
     printf("Enter command: ");
     scanf("%s", command);
-    printf("You selected command %s.\n", command);
+    printf("You have selected command %s.\n", command);
 
     if (strcmp(command, info) == 0){
       printf("COMP2211 Simplified Shell by sc16wyrw\n\n");
+    }
+
+    if (strcmp(command, pwd) == 0){
+      getcwd(cwd, sizeof(cwd));
+      printf("The current working directory is: %s\n", cwd);
     }
   }
 
