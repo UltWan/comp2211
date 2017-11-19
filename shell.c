dfc856e6c246dcf688c03b61870c6baea5e1a6c9
sc16wyrw <sc16wyrw@leeds.ac.uk>
2017-11-19 20:36:02 +0000

    info

--- shell.c
+++ shell.c
@@ -7,12 +7,16 @@ int main(int argc, char **argv)
 {
   char command[100];
   char exit[10] = "exit";
+  char info[10] = "info";
 
   while (strcmp(command, exit) != 0){
     printf("Enter command: ");
     scanf("%s", command);
-    printf("You selected command %s.\n\n", command);
+    printf("You selected command %s.\n", command);
 
+    if (strcmp(command, info) == 0){
+      printf("COMP2211 Simplified Shell by sc16wyrw\n\n");
+    }
   }
 
   printf("Exiting shell\n");
