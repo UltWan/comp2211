a394bc5f17c592bf0b23d43dfd15d12a640d368a
sc16wyrw <sc16wyrw@leeds.ac.uk>
2017-11-20 01:04:43 +0000

    Change working directory?

--- shell.c
+++ shell.c
@@ -7,26 +7,42 @@
 int main(int argc, char **argv)
 {
   char command[50];
-  char exit[5] = "exit";
-  char info[5] = "info";
-  char pwd[4]= "pwd";
+  char exit[6] = "exit\n";
+  char info[6] = "info\n";
+  char pwd[5]= "pwd\n";
   char cwd[50];
+  char cd[50]= "cd /";
+  char *chwd = command + 3;
 
+  // loop does not exit until exit command is entered
   while (strcmp(command, exit) != 0){
     printf("Enter command: ");
-    scanf("%s", command);
-    printf("You have selected command %s.\n", command);
+    fgets(command, sizeof command, stdin);
+    printf("You have selected command: %s", command);
 
     if (strcmp(command, info) == 0){
       printf("COMP2211 Simplified Shell by sc16wyrw\n\n");
     }
 
+    // gets and prints current working directory
     if (strcmp(command, pwd) == 0){
       getcwd(cwd, sizeof(cwd));
-      printf("The current working directory is: %s\n", cwd);
+      printf("The current working directory is: %s\n\n", cwd);
     }
-  }
 
+    // changes current working directory
+    if (strncmp(command, cd, 4) == 0){
+      chdir(chwd);
+      printf("5) The current working directory is: %s\n", chwd);
+      getcwd(chwd, sizeof(chwd));
+      printf("Switched current working directory to: %s\n\n", chwd);
+      strcpy(cwd, chwd);
+    }
+
+    /*else{
+      printf("No such command found\n\n");
+    }*/
+  }
   printf("Exiting shell\n");
   return 0;  
 }
