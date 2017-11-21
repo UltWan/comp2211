6452de205e1075533b2ef99b4668860de80245b6
sc16wyrw <sc16wyrw@leeds.ac.uk>
2017-11-21 00:46:53 +0000

    cd

--- shell.c
+++ shell.c
@@ -11,6 +11,7 @@ int main(int argc, char **argv)
   char info[6] = "info\n";
   char pwd[5]= "pwd\n";
   char cwd[50];
+  char temp[50] = "temp";
   char cd[50]= "cd /";
   char *chwd = command + 3;
 
@@ -21,27 +22,34 @@ int main(int argc, char **argv)
     printf("You have selected command: %s", command);
 
     if (strcmp(command, info) == 0){
-      printf("COMP2211 Simplified Shell by sc16wyrw\n\n");
+      printf("COMP2211 Simplified Shell by sc16wyrw\n");
     }
 
     // gets and prints current working directory
     if (strcmp(command, pwd) == 0){
-      getcwd(cwd, sizeof(cwd));
-      printf("The current working directory is: %s\n\n", cwd);
+      // if working directory has been changed
+      if (strcmp(cwd, temp) == 0){
+        get_current_dir_name(cwd);
+        printf("1) The current working directory is: %s\n", cwd);
+      }
+      // if working directory has not been changed
+      else{
+        getcwd(cwd, sizeof(cwd));
+        printf("2) The current working directory is: %s\n", cwd);
+      }
     }
 
     // changes current working directory
     if (strncmp(command, cd, 4) == 0){
       chdir(chwd);
-      printf("5) The current working directory is: %s\n", chwd);
-      getcwd(chwd, sizeof(chwd));
-      printf("Switched current working directory to: %s\n\n", chwd);
       strcpy(cwd, chwd);
+      strcpy(temp, chwd);
+      printf("Switched current working directory to: %s\n", cwd);
     }
 
-    /*else{
-      printf("No such command found\n\n");
-    }*/
+    else{
+      printf("No such command found\n");
+    }
   }
   printf("Exiting shell\n");
   return 0;  
